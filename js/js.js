1f7dc58c8a17eb30458b996a852147b9079fe818
Michael Lotkowski <michael@lotkowscy.co.uk>
2014-01-27 13:38:29 +0000

    carousel

--- js/js.js
+++ js/js.js
@@ -1,9 +1,19 @@
 
 
 $( document ).ready(function() {
-  $('div.jumbotron').css('height', $(window).height()-50);
-	$('div.jumbotron').fadeIn(1000, function(){
+  	$('div.carousel').css('height', $(window).height()-50);
+	$('div.item').css('height', $(window).height()-50);
+  	$('div.carousel').fadeIn(1000, function(){
 		$('nav').show('blind', 300);
 	});
+
+	$('.carousel').carousel({
+  interval: 3000
+});
 	//
 });
+
+window.onresize = function(event) {
+   $('div.carousel').css('height', $(window).height()-50);
+	$('div.item').css('height', $(window).height()-50);
+}
