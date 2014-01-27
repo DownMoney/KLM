7e85df4ef14ada9b74422be66fbaefe24e86a460
DownMoney <michael@lotkowscy.co.uk>
2014-01-27 12:09:21 +0000

    many

--- js/js.js
+++ js/js.js
@@ -1,3 +1,9 @@
+
+
 $( document ).ready(function() {
   $('div.jumbotron').css('height', $(window).height()-50);
+	$('div.jumbotron').fadeIn(1000, function(){
+		$('nav').show('blind', 300);
+	});
+	//
 });
