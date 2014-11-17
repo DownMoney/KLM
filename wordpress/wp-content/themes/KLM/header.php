e891804c5c6a99f99db267bcd4b00ed682299313
Michael Lotkowski <michael@lotkowscy.co.uk>
2014-11-17 00:53:21 +0000

    ga

--- wordpress/wp-content/themes/KLM/header.php
+++ wordpress/wp-content/themes/KLM/header.php
@@ -9,6 +9,17 @@
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/style.css"/>
 
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-49608567-1', 'klmchauffeurs.com');
+        ga('send', 'pageview');
+
+    </script>
+
     <?php wp_head(); ?>
 
 </head>
